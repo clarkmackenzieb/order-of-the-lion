96ff7fcb252f38bd77f142cdc174b15bed8cad03
Mackenzie <clarkmackenzieb@gmail.com>
2017-12-07 12:08:19 -0600

    things in wrong places

--- config.js
+++ config.js
@@ -1,11 +1,2 @@
-const express = requires('express');
-const cors = requires('cors');
-const {json} = require('body-parser');
-
-const port = 3005;
-
-const app = express();
-
-app.use(json())
-
-// app.use(express.static('./public'));
+node_modules/
+config/
